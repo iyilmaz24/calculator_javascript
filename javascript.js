e00ef08ecd2b9a39108bc702f2223a4b71b6ac99
Irfan Yilmaz <iy.usa@outlook.com>
2023-10-23 17:10:57 -0400

    add more functionality add html id attribute to "AC", "C", and "=" buttons replace "extraWideButton" class with id edit values of button elements for displaying user input with JS access and store all buttons as dom nodes through selectors in JS file

--- javascript.js
+++ javascript.js
@@ -4,12 +4,22 @@ window.addEventListener("DOMContentLoaded", () => {
     inputText = document.getElementById("inputText")
     outputText = document.getElementById("outputText")
 
-    buttons = document.getElementsByClassName("button");
+    buttons = document.getElementsByClassName("buttonDisplay");
     for(let i = 0; i < buttons.length; i++){
         buttons[i].addEventListener("click", (event) => {
-            console.log(event.target.value)
+            inputText.textContent += event.target.value
         })
     }
+
+    equalButton = document.getElementById("extraWideButton")
+
+    acButton = document.getElementById("ac-btn")
+
+    cButton = document.getElementById("c-btn")
+
+    console.log(equalButton)
+    console.log(acButton)
+    console.log(cButton)
 
 });
 
